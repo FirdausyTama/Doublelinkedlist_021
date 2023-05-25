44301eb87bc402fd28ee233e35d1613fe037a703
FirdausyTama <atama.firdausy@gmail.com>
2023-05-25 15:13:46 +0700

    Fungsi dari code tersebut adalah untuk melakukan seleksi kondisi berdasarkan nilai dari variabel ch dan menjalankan blok kode yang sesuai dengan nilai tersebut

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -179,6 +179,21 @@ int main() {
 			cin >> ch;
 
 			switch (ch) {
+			case '1' :
+				obj.addNode();
+				break;
+			case '2' :
+				obj.hapus();
+				break;
+			case '3' :
+				obj.ascending();
+			case '4' :
+				obj.descending();
+				break;
+			case '5' :
+				obj.searchData();
+			case '6' :
+				return 0;
 			}
 		}
 	}
