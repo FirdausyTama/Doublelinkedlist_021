10b888252dd892d811a1d3c233b34855483c2540
FirdausyTama <atama.firdausy@gmail.com>
2023-05-25 10:30:32 +0700

    membuat class doublelinkedlist berseta struktur datanya

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -9,3 +9,19 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+
+	DoubleLinkedList();
+	void addNode();
+	bool search(int ro11No, Node** previous, Node** current);
+	bool deleteNode(int ro11No);
+	bool listEmpty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchData();
+};
